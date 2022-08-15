bc14fba8637abe0a369981b3dbb14435f1f5efab
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-08-15 21:40:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def start(client, message):
 @app.on_message(filters.command(['mdisk']))
 async def mdisk(client, message):
     await client.send_chat_action(message.chat.id, "typing")
-    a = await bot.send_message(
+    a = await client.send_message(
             chat_id=message.chat.id,
             text=f"Processing…",
             reply_to_message_id=message.message_id
